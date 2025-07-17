94d50d2bd0b62561a3dc757cc9c7cf03c6ba2ffc
sartikmj <sartikmj@gmail.com>
2025-07-17 23:11:01 +0530

    Backend completely setup with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cros';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import clientRoutes from '/routes/client.js'
-import generalRoutes from '/routes/general.js'
-import managementRoutes from '/routes/management.js'
-import salesRoutes from '/routes/sales.js'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 /* CONFIGURATION */
 dotenv.config(); //dotenv.config(); is used to load environment variables from a .env file into process.env in Node.js.
@@ -27,3 +27,10 @@ app.use("/client", clientRoutes) // It will include all client routes
 app.use("/general", generalRoutes) //for grttting users and dashboard
 app.use("/management", managementRoutes) // It will include all management routes
 app.use("/sales", salesRoutes) // It will include all sales routes
+
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(PORT, ()=> console.log(`Server Port ${PORT}`))
+}).catch((err)=> console.log(`${err} did not connect.`))
